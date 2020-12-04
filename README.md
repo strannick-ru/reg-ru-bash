abc25ea80ee60cdb16bb13f369314286b7974c15
Eugene Chaykin <eugene.c@namesilo.dev>
2020-12-04 12:20:35 +0800

    Add hook and README

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # reg-ru-bash
 Bash hook for dehydrated Let's Encrypt client for Reg.ru
+
+У reg.ru принудительно выставлен TTL на все записи >= 1h, раньше это можно было менять через API, что и делал мой хук. 
+Сейчас не меняется никак, из-за чего приходится использовать отвратительные костыли.
+
+В общем, ситуация следующая — нужно в DNS создавать CNAME-запись на каждый домен, для которого нужно получать сертификаты с авторизацией по DNS.
+и эти CNAME направлять либо на какой-нибудь DNS-хостинг с нормальным API (например, Cloudflare), либо, если такой возможности нет, на свой сервер, на который ставить bind9, обслуживающий только эту зону.
+
+в `/etc/dehydrated/config/`
+добавляется это:
+```
+export REGRU_url="https://api.reg.ru/api/regru2/zone"
+export REGRU_user="логинрегру"
+export REGRU_pass="парольрегру"
+```
+
+и это:
+```
+export NS_secret="secret"
+export NS_pass="password"
+export NS_server="111.222.222.111"
+export NS_zone="auth.rokiroki.ru"
+```
+
+у меня сделано так:
+1. в reg.ru сделана запись NS, ведущая на мой сервер с bind9 (auth.rokiroki.ru)
+2. bind9 настроен на обслуживание этой доменной зоны
+3. хук идёт на reg.ru и добавляет там CNAME-записи вида _acme-challenge.rokiroki.ru, ссылающиеся на auth.rokiroki.ru
+4. дальше хук вписывает в bind9 текстовые записи, которые ждёт Let's Encrypt и на этом всё.
+
+т.е. нужно:
+1. сервер с bind9 ([настроенный примерно так](https://xn--90aeniddllys.xn--p1ai/dinamicheskoe-izmenenie-dns-zapisej-v-bind/), я уже не помню подробностей, главное, чтобы команда `nsupdate -k /etc/bind/key/dnsupdater.key` добавляла и удаляла записи)
+
+2. NS-запись в reg.ru, ведущая на этот сервер
+
+3. сам хук
+
+Пример использования:
+```
+dehydrated -c -t dns-01 -k '/var/lib/dehydrated/hooks/reg_ru_named.sh' -d rokiroki.ru -d *.rokiroki.ru
+```
---
+++ reg_ru_named.sh
@@ -0,0 +1,128 @@
+#!/usr/bin/env bash
+
+#
+# deploy a DNS challenge on reg.ru
+#
+
+set -e
+set -u
+set -o pipefail
+
+function deploy_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+    local SUBDOMAIN="_acme-challenge"
+    local NSUPDATE="nsupdate -k /etc/bind/key/dnsupdater.key" TTL=30
+
+    D_LEN=$( echo "$DOMAIN" | sed 's/\./\n/g' | wc -l )
+
+    echo "deploy_challenge called: ${DOMAIN}, ${SUBDOMAIN}, ${TOKEN_VALUE}, $D_LEN"
+
+    ### check for domain name correct and exist
+
+    if (( $D_LEN < 2 )); then echo "Wrong domain. Exit"; exit; fi
+    if (( $D_LEN > 2 ))
+    then
+      DOM=()
+      for ((i=1; i <= "$D_LEN"; i++))
+        do
+          DOM[$i]=$( echo "$DOMAIN" | cut -d'.' -f"$i" )
+        done
+
+      let "DOM2 = $D_LEN - 1"
+      DOMAIN1=$( echo "${DOM[$DOM2]}.${DOM[$D_LEN]}" )
+    else
+      DOMAIN1="${1}"
+    fi
+
+    # add CNAME for _acme-challenge
+    SUBDOMAIN1=$( echo "${DOMAIN}" | sed "s/^\*\.//g" | sed "s/${DOMAIN1}//g" )
+
+    curl -X POST "$REGRU_url/add_cname" --data "username=$REGRU_user&password=$REGRU_pass&output_content_type=plain&show_input_params=0&domain_name=$DOMAIN1&input_format=json&input_data={\"domains\":[{\"dname\":\"$DOMAIN1\"}],\"subdomain\":\"$SUBDOMAIN.$SUBDOMAIN1$DOMAIN1.\",\"canonical_name\":\"$NS_zone\"}" > /dev/null 2>&1
+
+    # use force of local bind9, Luke!
+    printf "server %s\nupdate add %s. %d in TXT \"%s\"\nsend\n" "${NS_server}" "${NS_zone}" "${TTL}" "${TOKEN_VALUE}" | $NSUPDATE
+
+    if [ "${SUBDOMAIN1}" != "" ]
+    then
+        SUBDOMAIN1=""
+        curl -X POST "$REGRU_url/add_cname" --data "username=$REGRU_user&password=$REGRU_pass&output_content_type=plain&show_input_params=0&domain_name=$DOMAIN1&input_format=json&input_data={\"domains\":[{\"dname\":\"$DOMAIN1\"}],\"subdomain\":\"$SUBDOMAIN.$SUBDOMAIN1$DOMAIN1.\",\"canonical_name\":\"$NS_zone\"}" > /dev/null 2>&1
+        printf "server %s\nupdate add %s. %d in TXT \"%s\"\nsend\n" "${NS_server}" "${NS_zone}" "${TTL}" "${TOKEN_VALUE}" | $NSUPDATE
+    fi
+
+    secs=$((1 * 60))
+    while [ $secs -gt 0 ]; do
+       echo -ne "$secs\033[0K\r"
+       sleep 1
+       : $((secs--))
+    done
+}
+
+function clean_challenge {
+    local DOMAIN="${1}" TOKEN_FILENAME="${2}" TOKEN_VALUE="${3}"
+    local SUBDOMAIN="_acme-challenge"
+    local NSUPDATE="nsupdate -k /etc/bind/key/dnsupdater.key" TTL=30
+
+    D_LEN=$( echo "$DOMAIN" | sed 's/\./\n/g' | wc -l )
+
+    echo "clean_challenge called: ${DOMAIN}, ${SUBDOMAIN}, ${TOKEN_VALUE}, $D_LEN"
+
+    ### check for domain name correct and exist
+
+    if (( $D_LEN < 2 )); then echo "Wrong domain. Exit"; exit; fi
+    if (( $D_LEN > 2 ))
+    then
+      DOM=()
+      for ((i=1; i <= "$D_LEN"; i++))
+        do
+          DOM[$i]=$( echo "$DOMAIN" | cut -d'.' -f"$i" )
+        done
+
+      let "DOM2 = $D_LEN - 1"
+      DOMAIN1=$( echo "${DOM[$DOM2]}.${DOM[$D_LEN]}" )
+    else
+      DOMAIN1="${1}"
+    fi
+
+    RR=$( curl --silent -X POST "$REGRU_url/get_resource_records" --data "username=$REGRU_user&password=$REGRU_pass&output_content_type=plain&show_input_params=0&domain_name=$DOMAIN1&input_format=json&input_data={\"domains\":[{\"dname\":\"$DOMAIN1\"}]}" )
+
+    ### Clean up all _acme-challenge records
+    for i in $( echo "${RR}" | grep "$SUBDOMAIN" | grep "subname" | cut -d'"' -f4 )
+      do
+        RECTYPE=$( echo "${RR}" | grep -B2 "${i}\"" | grep "rectype" | cut -d'"' -f4 )
+        SUBNAME=$( echo "${RR}" | grep "${i}\"" | cut -d'"' -f4 )
+        curl -X POST "$REGRU_url/remove_record" --data "username=$REGRU_user&password=$REGRU_pass&output_content_type=plain&show_input_params=0&domain_name=$DOMAIN1&input_format=json&input_data={\"domains\":[{\"dname\":\"$DOMAIN1\"}],\"subdomain\":\"$SUBNAME\",\"record_type\":\"$RECTYPE\"}" > /dev/null 2>&1
+      done
+
+    printf "server %s\nupdate delete %s. %d in TXT \"%s\"\nsend\n" "${NS_server}" "${NS_zone}" "${TTL}" | $NSUPDATE
+}
+
+function invalid_challenge() {
+    local DOMAIN="${1}" RESPONSE="${2}"
+
+    echo "invalid_challenge called: ${DOMAIN}, ${RESPONSE}"
+}
+
+function deploy_cert {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+
+    echo "deploy_cert called: ${DOMAIN}, ${KEYFILE}, ${CERTFILE}, ${FULLCHAINFILE}, ${CHAINFILE}"
+}
+
+function unchanged_cert {
+    local DOMAIN="${1}" KEYFILE="${2}" CERTFILE="${3}" FULLCHAINFILE="${4}" CHAINFILE="${5}"
+
+    echo "unchanged_cert called: ${DOMAIN}, ${KEYFILE}, ${CERTFILE}, ${FULLCHAINFILE}, ${CHAINFILE}"
+}
+
+exit_hook() {
+  :
+}
+
+startup_hook() {
+  :
+}
+
+HANDLER=$1; shift;
+if [ -n "$(type -t $HANDLER)" ] && [ "$(type -t $HANDLER)" = function ]; then
+  $HANDLER "$@"
+fi
